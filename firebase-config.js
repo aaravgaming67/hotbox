a8692ce8dd876e9f6b7273ebf098819068b9097d
aaravgaming67 <aarav.kudva67@gmail.com>
2026-05-22 23:03:58 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjJaKI6SXNRlTMhrKIGcDVNOVHUxsjEO4",
+  authDomain: "hotboxgaming-51dc2.firebaseapp.com",
+  projectId: "hotboxgaming-51dc2",
+  storageBucket: "hotboxgaming-51dc2.firebasestorage.app",
+  messagingSenderId: "167301436788",
+  appId: "1:167301436788:web:8ca699ceab337408060821",
+  measurementId: "G-1ZFGY4BJW2"
+};
+
+export const firebaseCollection = "bookings";
